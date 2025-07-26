e0309aaeefb96ee626ad46cd3882fbdfb72dbef6
Sanchit Tikhile <157561024+Itachi07l@users.noreply.github.com>
2025-07-26 13:45:37 +0530

    Update ProductDetails.jsx

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import Footer from './Footer'
-import InfiniteMenu from '../Components/infiniteMenu'
+import InfiniteMenu from '../Components/InfiniteMenu'
 
 const ProductDetails = ({ product }) => {
   const items = [
